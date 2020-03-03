27db018572beb028f39db9cba0bcec6742a61319
matthingly-git <matthew.cesar96@gmail.com>
2020-03-03 18:46:58 +0100

    everyBase e filterBase ok

--- every.js
+++ every.js
@@ -1,1 +1,23 @@
+//EVERYBASE
+function everyBase(array, callBackF) {
+  return callBackF(array)
+}
 
+function even(array) {
+  let result = null;
+  let newArray = [];
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] % 2 === 0) {
+      newArray.push(array[i])
+    }
+  }
+  if (newArray.length === array.length) {
+    result = true
+  }
+  return result;
+}
+
+let arrayProva = [2, 4];
+
+
+console.log(everyBase(arrayProva, even))
--- filter.js
+++ filter.js
@@ -1,1 +1,18 @@
+//FILTERBASE
+function filterBase(array, callbackF) {
+  return callbackF(array)
+}
 
+function even(array) {
+  let newArray = [];
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] % 2 === 0) {
+      newArray.push(array[i])
+    }
+  }
+  return newArray;
+}
+
+let arrayProva = [1, 2, 3, 4, 5, 346, 78, 45, 67, 90];
+
+console.log(filterBase(arrayProva, even))
