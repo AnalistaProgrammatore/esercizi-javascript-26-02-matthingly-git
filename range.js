c5f178d75ca9f7c9a3909d81fff48edf179afd85
matthingly-git <matthew.cesar96@gmail.com>
2020-03-03 17:56:27 +0100

    100%

--- range.js
+++ range.js
@@ -1,3 +1,19 @@
 /**
 * Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
 **/
+
+
+//FUNZIONE RANGE CON VALORI POSIZIONALI
+const rangePositional = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+
+console.log (rangePositional(1, 27, 2));
+
+
+
+//FUNZIONE RANGE CON VALORI NOMINALI
+const rangeNamed = ({start = 1, stop = 27, step = 27}) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+
+let start = 1;
+let stop = 27;
+let step = 4;
+console.log(rangeNamed({step, start, stop}));
