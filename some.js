a13929552d4fd1b9b5dc65a477f21d3ad4f1fdb3
matthingly-git <matthew.cesar96@gmail.com>
2020-03-03 18:41:26 +0100

    Fix Somebase

--- some.js
+++ some.js
@@ -1,1 +1,19 @@
+//SOMEBASE
+function someBase(array, callBackF) {
+  return callBackF(array)
+}
 
+function even(array) {
+  let result = null;
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] % 2 === 0) {
+      result = true;
+    }
+  }
+  return result;
+}
+
+let arrayProva = [1, 2, 3, 4, 5];
+
+
+console.log(someBase(arrayProva, even))
