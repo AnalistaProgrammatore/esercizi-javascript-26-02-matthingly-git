9126824a3337fcd206153d48ceb8056406ac8773
matthingly-git <matthew.cesar96@gmail.com>
2020-03-04 09:58:21 +0100

    ok

--- some.js
+++ some.js
@@ -1,4 +1,5 @@
 //SOMEBASE
+/*
 function someBase(array, callBackF) {
   return callBackF(array)
 }
@@ -17,3 +18,28 @@ let arrayProva = [1, 2, 3, 4, 5];
 
 
 console.log(someBase(arrayProva, even))
+*/
+
+//SOMEBASE MODO CORRETTO
+const someBase = function (arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    const result = callback(arr[i])
+    if (result) return true
+  }
+  return false
+}
+
+
+//SOMEREDUCE
+const someReduce = function (arr, callback) {
+  return arr.reduce((acc, cur) => {
+    if (acc) return acc
+    return callback(cur)
+  }, false)
+}
+
+const myCallback = x => x > 15
+const A = [1, 2, 3, 4, 5]
+
+console.log(someBase(A, myCallback))
+console.log(someReduce(A, myCallback))
